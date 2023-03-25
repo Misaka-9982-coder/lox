0d531ea9ab8e02beee0154a68aea5ae36615afa3
misaka <1784321894@qq.com>
2023-03-25 16:20:19 +0800

    feat: Add support for if-else statements.

--- src/main/java/lox/Parser.java
+++ src/main/java/lox/Parser.java
@@ -55,6 +55,20 @@ class Parser {
         return expressionStatement();
     }
 
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+        Stmt thenBranch = statement();
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
+    }
+
     private Stmt printStatement() {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
