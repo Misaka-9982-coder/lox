225500aa2d211fa2f7db759f24d7d0b2ee956862
misaka <1784321894@qq.com>
2023-03-29 21:12:21 +0800

    feat: Implement LoxClass and LoxInstance with LoxCallable interface.

--- jlox/src/main/java/com/craftinginterpreters/lox/LoxClass.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/LoxClass.java
@@ -1,6 +1,8 @@
 package com.craftinginterpreters.lox;
 
-class LoxClass {
+import java.util.List;
+
+class LoxClass implements LoxCallable {
     final String name;
 
     LoxClass(String name) {
@@ -10,5 +12,17 @@ class LoxClass {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter,
+                       List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
+    }
+
+    @Override
+    public int arity() {
+        return 0;
     }
 }
