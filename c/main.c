ca15300ec5579ca568958299cb0cbda13c885981
misaka <1784321894@qq.com>
2023-03-31 19:50:18 +0800

    Add support for negation operator in bytecode interpreter.

--- c/main.c
+++ c/main.c
@@ -2,16 +2,23 @@
 #include "chunk.h"
 #include "debug.h"
 
+#include "vm.h"
+
 int main(int argc, char *argv[]) {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
-
+    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+
+    interpret(&chunk);
+
+    freeVM();
 
     freeChunk(&chunk);
 
