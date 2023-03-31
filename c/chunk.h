67fb13501581e457a1a513060c03f7a30de61945
misaka <1784321894@qq.com>
2023-03-31 15:06:05 +0800

    feat: Add support for constant values in bytecode.

--- c/chunk.h
+++ c/chunk.h
@@ -2,8 +2,10 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -12,12 +14,16 @@ typedef struct {
     int capacity;
 
     uint8_t* code;
+    int* lines;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 
 void freeChunk(Chunk* chunk);
 
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+
+int addConstant(Chunk* chunk, Value value);
 
 #endif
