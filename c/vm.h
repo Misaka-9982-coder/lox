ff5a6c759de30d230eee62f7a47917c4e9a33ccf
misaka <1784321894@qq.com>
2023-04-01 16:08:47 +0800

    Add object struct and string handling functions

--- c/vm.h
+++ c/vm.h
@@ -12,6 +12,7 @@ typedef struct {
 
     Value stack[STACK_MAX];
     Value* stackTop;
+    Obj* objects;
 } VM;
 
 typedef enum {
@@ -19,6 +20,8 @@ typedef enum {
     INTERPRET_COMPILE_ERROR,
     INTERPRET_RUNTIME_ERROR
 } InterpretResult;
+
+extern VM vm;
 
 void initVM();
 void freeVM();
